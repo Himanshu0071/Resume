ed1bc038eda518136a821d8a8205b13b216879c6
Himanshu Sharma <sh.himanshu.0071@gmail.com>
2022-08-02 01:23:54 +0530

    added whatsapp

---
+++ src/PortfolioContainer/Whatsapp/WhatsApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './WhatsApp.css'
+
+export default function WhatsApp() {
+  return (
+    <div className='container'> 
+    <a href='https://wa.me/918743989940'>
+        <span> WhatsApp Me</span>
+        <i class="fa fa-whatsapp fa-2x" aria-hidden="true"></i>
+    </a>
+</div>
+  )
+}
+
